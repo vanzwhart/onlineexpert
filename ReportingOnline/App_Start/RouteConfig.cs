f34b70655eec3a65705d7ac4fc3f810934e33ccb
irvan.kurniawan <irvan.kurniawan@JKTNE1601086>
2020-03-06 16:50:37 +0700

    Landing Page

--- ReportingOnline/App_Start/RouteConfig.cs
+++ ReportingOnline/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ReportingOnline
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Landing", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ReportingOnline/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using ReportingOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportingOnline.Controllers
+{
+   
+    public class ShopController : Controller
+    {
+       
+        // GET: Shop
+        
+        public ActionResult Index()
+        {
+            //var userid = _context.Users.FirstOrDefault(a => a.UserName == User.Identity.Name).Id;
+            //IList<MsOnlineShop> shop = _context.OnlineShop.Where(x => x.UserID == userid).ToList();
+            return View();
+        }
+    }
+}
